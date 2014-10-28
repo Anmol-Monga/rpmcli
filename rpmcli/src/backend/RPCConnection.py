5c22580f23053863a516a2b8d4c03222f0604533
Daniel Casper <dancasper@gmail.com>
2014-10-28 12:48:19 -0600

    Clean up write method / debug output.
    The Write Method on Telnet doubles IAC control characters
    (\xff) which is unnecessary on this client.  Also, better
    use of the built in debug output from the Telnet Class.

--- rpmcli/src/backend/RPCConnection.py
+++ rpmcli/src/backend/RPCConnection.py
@@ -30,11 +30,11 @@ class RPCConnection(object, Telnet):
     self._disconnect()
 
   def send(self, adict):
-    print json.dumps(adict)
     self.write(json.dumps(adict))
 
   def recv(self):
     resp = self.read_until('\n')
+    self.msg(resp)
     try:
       return json.loads(resp)
     except Exception, e:
@@ -45,6 +45,20 @@ class RPCConnection(object, Telnet):
   def comm(self, adict):
     self.send(adict)
     return self.recv()
+
+  def write(self, buffer):
+    """Write a string to the socket.
+
+    Can block if the connection is blocked.  May raise
+    socket.error if the connection is closed.
+
+     - Note this is overridden from the Telnet class to prevent IAC (\xFF)
+       duplication which is unnecessary for this connection.
+    """
+    self.msg("send %r", buffer)
+    self.sock.sendall(buffer)
+
+
 
 if __name__ == "__main__":
   with RPCConnection() as conn:
